7cd66551e9d4789911fa3a6be87277fd3662f6cf
malfattif@yahoo.com.br <malfattif@yahoo.com.br>
2019-06-08 13:18:25 -0300

    MCA

--- data_frames.R
+++ data_frames.R
@@ -8,13 +8,21 @@ nrow(mydata1) # number of rows of data frame
 myfulldata = rbind(mydata1, mydata2) # append files
 print(nrow(myfulldata))
 
-# MAYBE THIS IS NOT A CLUSTERIZATION PROBLEM!?
-
 install.packages("fastDummies") # install dummies
 library(fastDummies) # import dummies
 
 categoricalDataFrame = read.csv(file.path("~/r-worksheet/example_categorical.csv"), header=T)
+summary(categoricalDataFrame) #VERIFICAR A QUALIDADE DOS DOS DADOS
 dummies = fastDummies::dummy_cols(categoricalDataFrame)
 
+#dummie variables
 print(dummies)
+
+# MAYBE THIS IS NOT A CLUSTERIZATION PROBLEM!?
+# ANÁLISE DE CORRESPONDÊNCIA MULTIPLA (MCA)
+
+install.packages(c("FactoMineR", "factoextra"), dependencies=TRUE)
+library("FactoMineR") #MCA
+library("factoextra") #PLOT
+
 
