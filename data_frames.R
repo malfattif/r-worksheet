1119ea11ab0e706d7b905c37c0e00ea8b8823086
malfattif@yahoo.com.br <malfattif@yahoo.com.br>
2019-06-08 12:36:06 -0300

    Dummies dataframe

--- data_frames.R
+++ data_frames.R
@@ -1,12 +1,20 @@
-
-mydata1 = read.csv(file.path("example.csv"), header=T)
-
+mydata1 = read.csv(file.path("example.csv"), header=T) # import CSV
 mydata2 = read.csv(file.path("example.csv"), header=T)
 
-head(mydata1)
-tail(mydata1)
-nrow(mydata1)
+head(mydata1) # first 5 rows of data frame
+tail(mydata1) # last 5 rows of data frame
+nrow(mydata1) # number of rows of data frame
 
 myfulldata = rbind(mydata1, mydata2) # append files
 print(nrow(myfulldata))
+
+# MAYBE THIS IS NOT A CLUSTERIZATION PROBLEM!?
+
+install.packages("fastDummies") # install dummies
+library(fastDummies) # import dummies
+
+categoricalDataFrame = read.csv(file.path("~/r-worksheet/example_categorical.csv"), header=T)
+dummies = fastDummies::dummy_cols(categoricalDataFrame)
+
+print(dummies)
 
