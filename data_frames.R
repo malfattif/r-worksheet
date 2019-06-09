6061221349465a4b3997c38d5cd9020e6081849a
malfattif@yahoo.com.br <malfattif@yahoo.com.br>
2019-06-09 14:41:55 -0300

    fixing workdir

--- data_frames.R
+++ data_frames.R
@@ -1,5 +1,8 @@
-mydata1 = read.csv(file.path("example.csv"), header=T) # import CSV
-mydata2 = read.csv(file.path("example.csv"), header=T)
+setwd(paste(getwd(), "/r-worksheet", sep = "")) # SETING WORK DIRECTORY
+getwd() # GET WORKING DIRECTORY
+
+mydata1 = read.csv("example.csv") # import CSV
+mydata2 = read.csv("example.csv")
 
 head(mydata1) # first 5 rows of data frame
 tail(mydata1) # last 5 rows of data frame
@@ -11,18 +14,14 @@ print(nrow(myfulldata))
 install.packages("fastDummies") # install dummies
 library(fastDummies) # import dummies
 
-categoricalDataFrame = read.csv(file.path("~/r-worksheet/example_categorical.csv"), header=T)
+categoricalDataFrame = read.csv("example_categorical.csv")
 summary(categoricalDataFrame) #VERIFICAR A QUALIDADE DOS DOS DADOS
 dummies = fastDummies::dummy_cols(categoricalDataFrame)
 
 #dummie variables
 print(dummies)
 
-# MAYBE THIS IS NOT A CLUSTERIZATION PROBLEM!?
 # ANÁLISE DE CORRESPONDÊNCIA MULTIPLA (MCA)
-
 install.packages(c("FactoMineR", "factoextra"), dependencies=TRUE)
 library("FactoMineR") #MCA
 library("factoextra") #PLOT
-
-
