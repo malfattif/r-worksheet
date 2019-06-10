5655cf01a0e3c0e4f36d7e0d87ea9cd653885dde
malfattif@yahoo.com.br <malfattif@yahoo.com.br>
2019-06-10 17:33:15 -0300

    read all files from directory

--- data_frames.R
+++ data_frames.R
@@ -1,9 +1,12 @@
 setwd(paste(getwd(), "/r-worksheet", sep = "")) # SETING WORK DIRECTORY
 getwd() # GET WORKING DIRECTORY
 
-TSEFiles =list.files(path = ".", pattern = "*.csv|*.txt") #GET ALL TXT AN CSV FILES
+TSEFiles = list.files(path = "votacao/", pattern = "*.csv|*.txt")  #GET ALL TXT AN CSV FILES
 
-tables <- lapply(TSEFiles, read.csv, header = TRUE) #review this
+#acreElections = read.csv("votacao/votacao_candidato_munzona_2016_AC.csv", sep = ";") # import CSV
+
+setwd(paste(getwd(), "/votacao", sep = "")) 
+tables <- lapply(TSEFiles, read.csv, sep = ";", header=TRUE, stringsAsFactors=FALSE, fileEncoding="latin1")
 
 mydata1 = read.csv("example.csv") # import CSV
 mydata2 = read.csv("example.csv")
