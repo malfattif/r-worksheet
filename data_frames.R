315635e243b4f1aae90c49fa50543c8374317e3f
malfattif@yahoo.com.br <malfattif@yahoo.com.br>
2019-06-10 16:30:05 -0300

    read all directory files (.txt and .csv)

--- data_frames.R
+++ data_frames.R
@@ -1,6 +1,10 @@
 setwd(paste(getwd(), "/r-worksheet", sep = "")) # SETING WORK DIRECTORY
 getwd() # GET WORKING DIRECTORY
 
+TSEFiles =list.files(path = ".", pattern = "*.csv|*.txt") #GET ALL TXT AN CSV FILES
+
+tables <- lapply(TSEFiles, read.csv, header = TRUE) #review this
+
 mydata1 = read.csv("example.csv") # import CSV
 mydata2 = read.csv("example.csv")
 
@@ -8,7 +12,7 @@ head(mydata1) # first 5 rows of data frame
 tail(mydata1) # last 5 rows of data frame
 nrow(mydata1) # number of rows of data frame
 
-myfulldata = rbind(mydata1, mydata2) # append files
+myfulldata = rbind(mydata1, mydata2) # append files 
 print(nrow(myfulldata))
 
 install.packages("fastDummies") # install dummies
@@ -22,6 +26,12 @@ dummies = fastDummies::dummy_cols(categoricalDataFrame)
 print(dummies)
 
 # ANÁLISE DE CORRESPONDÊNCIA MULTIPLA (MCA)
-install.packages(c("FactoMineR", "factoextra"), dependencies=TRUE)
-library("FactoMineR") #MCA
-library("factoextra") #PLOT
+#install.packages(c("FactoMineR", "factoextra"), dependencies=TRUE)
+#library("FactoMineR") #MCA
+#library("factoextra") #PLOT
+
+## install.packages(c('tibble', 'dplyr', 'tidyr'))
+#library(tibble)
+#library(dplyr)
+#library(tidyr)
+
