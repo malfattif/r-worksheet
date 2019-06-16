426650f4a708a118cc13ed8ca7ce6a992185e8de
Felipe Malfatti <malfattif@yahoo.com.br>
2019-06-16 15:49:52 -0300

    more operation on dataframe

--- data_frames.R
+++ data_frames.R
@@ -1,20 +1,18 @@
 # WORK DIRECTORY 
 # LEITURA DE ARQUIVOS
-# UNIFICA√á√ÉO EM UM DATAFRAME
+# UNIFICA«√O EM UM DATAFRAME
 # LEITURA DE DATAFRAMES
-# COME√áAREMOS EM ORDEM DESCENDENTE POR ANO (TEORICAMENTE TEM MAIS DADOS)
-# VARI√ÅVEIS CATEGORICAS
-# APRENDIZADO SUPERVISIONADO OU N√ÉO, REGRAS DE ASSOCIA√á√ÉO!? 
-# https://shapeofdata.wordpress.com/2014/03/04/k-modes/
+# COME«AREMOS EM ORDEM DESCENDENTE POR ANO (TEORICAMENTE TEM MAIS DADOS)
+# VARI√VEIS CATEGORICAS
 
-setwd(paste(getwd(), "/r-worksheet/votacao", sep = ""))  # SETING WORK DIRECTORY
+setwd("C:/Users/1513 IRON/Desktop/Projetos/ProjetosGIT/r-worksheet/votacao") # SETING WORK DIRECTORY
 getwd() # GET WORKING DIRECTORY
 
 #acreElections = read.csv("votacao_candidato_munzona_2016_AC.csv", sep = ";") # import CSV
-TSEFiles = list.files(path = ".", pattern = "*.csv|*.txt")  #GET ALL TXT AN CSV FILES
-  
 
-for (file in TSEFiles){
+TSEFiles = list.files(path = ".", pattern = "*.csv|*.txt")  #GET ALL TXT AN CSV FILES
+
+for (file in TSEFiles) {
   # if the merged dataset doesn't exist, create it
   if (!exists("dataset")){
     dataset <- read.csv(file, sep=";", header=TRUE, stringsAsFactors=FALSE, fileEncoding="latin1")
@@ -22,23 +20,68 @@ for (file in TSEFiles){
   # if the merged dataset does exist, append to it
   else if (exists("dataset")){
     temp_dataset <- read.csv(file, sep=";", header=TRUE, stringsAsFactors=FALSE, fileEncoding="latin1")
-    dataset<-rbind(dataset, temp_dataset)
+    dataset<- rbind(dataset, temp_dataset)
     rm(temp_dataset)
   }
 }
 
 nrow(dataset) # number of rows of data frame
-head(dataset[1, 1:10]) # first 5 rows of data frame
-tail(dataset[1, 1:10]) # last 5 rows of data frame
+head(dataset[1,]) # first 5 rows of data frame
+tail(dataset[1, 1:10]) # last 5 rows of data frame get only 10 columns
 
 unique(dataset["SG_UF"]) # SHOW ALL UF
 summary(dataset["QT_VOTOS_NOMINAIS"]) # SHOW SUMMARY OF VOTES
 
+#SHOW SUM OF ALL VOTES
+sum(dataset["QT_VOTOS_NOMINAIS"], na.rm=TRUE)
+
+#SHOW MAX OF ALL VOTES
+max(dataset["QT_VOTOS_NOMINAIS"], na.rm=TRUE)
+
+#FILTERING A DATAFRAME
+onlyAcre <- dataset$SG_UF == "AC" #CREATE A LOGICAL TYPE
+datasetOnlyAcre = dataset[onlyAcre,] #DO THE FILTER
+
+#FILTERING A DATAFRAME
+onlyWithMoreThan70kVotes <- dataset$QT_VOTOS_NOMINAIS > 70000
+datasetMoreThan70kVotes = dataset[onlyWithMoreThan70kVotes,]
+
+#FILTERING A DATAFRAME USING AND (&)
+onlyElectedWithMoreThan70kVotes <- dataset$QT_VOTOS_NOMINAIS > 70000 & dataset$DS_SIT_TOT_TURNO == 'ELEITO'
+datasetElectedMoreThan70kVotes = dataset[onlyElectedWithMoreThan70kVotes,]
+
+#FILTERING A DATAFRAME USING OR (|)
+onlyPTandPMDBCandidates <- dataset$SG_PARTIDO == "PT" | dataset$SG_PARTIDO == 'PMDB'
+datasetPTandPmdb = dataset[onlyPTandPMDBCandidates,]
+unique(datasetPTandPmdb["SG_PARTIDO"])
+
+#THE MOST VOTED 
+mostVoted <- dataset$QT_VOTOS_NOMINAIS == max(dataset["QT_VOTOS_NOMINAIS"], na.rm=TRUE)
+datasetMostVoted = dataset[mostVoted,]
+
+
+#GROUPING AND SUM DATASET
+sumVotes = aggregate(dataset$QT_VOTOS_NOMINAIS, by=list(dataset$NM_CANDIDATO), FUN=sum)
+onlyDoria = sumVotes$Group.1 == "JO√O AGRIPINO DA COSTA DORIA JUNIOR"
+doria = sumVotes[onlyDoria,]
 
 # EXCLUDE HIGH CORRELATIONS COLUMNS SG_UF, NM_MUNICIP IO, NM_PARTIDO
 excludeColumns <- names(dataset) %in% c("SG_UF", "NM_MUNICIPIO", "NM_PARTIDO") 
 newDataset <- dataset[!excludeColumns]
 
+
+#GETTING A SAMPLE FROM DATAFRAME
+install.packages("dplyr")
+library(dplyr)
+
+sampleSize = nrow(dataset) * 0.25 #GETTING 25% PERCENT FROM THE ORIGINAL DATASET
+sampleDataset = sample_n(dataset, sampleSize)
+
+print(nrow(dataset)) # full dataset
+print(nrow(sampleDataset)) # 25% of dataset (random data)
+
+
+#USIGN K MODES
 install.packages("klaR")  
 library(klaR)
 categoricalColumns = excludeColumns <- names(dataset) %in% c("CD_TIPO_ELEICAO", "CD_MUNICIPIO", "DT_GERACAO", "SQ_CANDIDATO", "HH_GERACAO", "ANO_ELEICAO", "NR_TURNO", "CD_ELEICAO", "SG_UE", "NR_ZONA", "CD_CARGO", "CD_SITUACAO_CANDIDATURA", "CD_DETALHE_SITUACAO_CAND", "NR_PARTIDO", "SQ_COLIGACAO", "CD_SIT_TOT_TURNO", "QT_VOTOS_NOMINAIS") 
@@ -69,4 +112,6 @@ print(dummies)
 #library(tibble)
 #library(dplyr)
 #library(tidyr)
+# APRENDIZADO SUPERVISIONADO OU N√O, REGRAS DE ASSOCIA«√O!? 
+# https://shapeofdata.wordpress.com/2014/03/04/k-modes/
 
